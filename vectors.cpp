2cd53070e2eaff0533afb2ac59eef6948eadf7b4
LucasAlbl <lucas.alblas@gmail.com>
2019-10-30 10:35:33 +0100

    added overloaded * + and - functions

--- vectors.cpp
+++ vectors.cpp
@@ -5,26 +5,94 @@
 #include "math.hpp"
 #include "basic_includes.hpp"
 
+std::ostream& operator<<(std::ostream &os, Vector2D const &v) {
+	return os << '{' << v.x << ',' << v.y << '}';
+}
+
+Vector2D operator+ (Vector2D &v, Vector2D &w) {
+	Vector2D temp;
+	temp.x = v.x + w.x;
+	temp.y = v.y + w.y;
+	return temp;
+}
+
+Vector2D operator- (Vector2D &v, Vector2D &w) {
+	Vector2D temp;
+	temp.x = v.x - w.x;
+	temp.y = v.y - w.y;
+	return temp;
+}
+
+Vector2D operator* (int a, Vector2D &w) {
+	Vector2D temp;
+	temp.x = w.x * a;
+	temp.y = w.y * a;
+	return temp;
+}
+
+Vector2D operator* (float a, Vector2D &w) {
+	Vector2D temp;
+	temp.x = w.x * a;
+	temp.y = w.y * a;
+	return temp;
+}
+
+Vector2D operator* (double a, Vector2D &w) {
+	Vector2D temp;
+	temp.x = w.x * a;
+	temp.y = w.y * a;
+	return temp;
+}
+
+Vector2D operator* (Vector2D &v, int &b) {
+	Vector2D temp;
+	temp.x = v.x * b;
+	temp.y = v.y * b;
+	return temp;
+}
+
+Vector2D operator* (Vector2D &v, float &b) {
+	Vector2D temp;
+	temp.x = v.x * b;
+	temp.y = v.y * b;
+	return temp;
+}
+
+Vector2D operator* (Vector2D &v, double &b) {
+	Vector2D temp;
+	temp.x = v.x * b;
+	temp.y = v.y * b;
+	return temp;
+}
+
 Vector2D::Vector2D(){
 	x = 0.0;
 	y = 0.0;
 }
 
-Vector2D::Vector2D(float a, float b){
+Vector2D::Vector2D(int a, int &b){
 	x = a;
 	y = b;
 }
 
-Vector2D::Vector2D(double a, double b) {
+Vector2D::Vector2D(float a, float &b){
 	x = a;
 	y = b;
 }
 
-std::ostream& operator<<(std::ostream &os, Vector2D const &v) {
-	return os << '{' << v.x << ',' << v.y << '}';
+Vector2D::Vector2D(double &a, double &b) {
+	x = a;
+	y = b;
 }
 
-double Vector2D::DotProduct(Vector2D v, Vector2D w) {
+Vector2D::Vector2D(std::vector<double> v) {
+	x = v.at(0);
+	y = v.at(1);
+}
+
+
+
+double Vector2D::DotProduct(Vector2D &v, Vector2D &w) {
 	double ret_val;
 	ret_val = v.x*w.x + v.y*w.y;
 	return ret_val;
--- vectors.hpp
+++ vectors.hpp
@@ -9,12 +9,24 @@
 
 class Vector2D {
 	friend std::ostream& operator<<(std::ostream &os, Vector2D const &v);
+	friend Vector2D operator* (int a, Vector2D &w);
+	friend Vector2D operator* (float a, Vector2D &w);
+	friend Vector2D operator* (double a, Vector2D &w);
+	friend Vector2D operator* (Vector2D &v, int &b);
+	friend Vector2D operator* (Vector2D &v, float &b);
+	friend Vector2D operator* (Vector2D &v, double &b);
+	friend Vector2D operator+ (Vector2D &v, Vector2D &w);
+	friend Vector2D operator- (Vector2D &v, Vector2D &w);
+
+
 public:
 	double x = 0,y = 0;
 	Vector2D();
-	Vector2D(float, float);
-	Vector2D(double, double);
-	static double DotProduct(Vector2D, Vector2D);
+	Vector2D(int a, int &b);
+	Vector2D(float a, float &b);
+	Vector2D(double &a, double &b);
+	explicit Vector2D(std::vector<double> v);
+	static double DotProduct(Vector2D &v, Vector2D &w);
 
 };
 
